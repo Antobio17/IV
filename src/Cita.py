0734c78cb81bee82380399b2d2f536be5ce56f43
argelion14 <argelion14@gmail.com>
2021-10-22 12:49:56 +0200

    Actualizo el yaml

--- src/Cita.py
+++ src/Cita.py
@@ -1,26 +1,91 @@
 from datetime import datetime
 
 class Cita:
+    """
+    Entidad que representa una cita 
+
+    Attributes
+    ----------
+    telefono : String
+        Número de telefono
+    dia : datetime
+        Tipo datetime que representa la fecha de la cita
+    turno : String
+        Cadena de texto que almacena el turno en el que se 
+        dará la cita, siendo posible tomar los valores:
+        mañana o tarde
+    tipo_de_cita : dict
+        Diccionario que almacenará el tipo de tratamiento, 
+        así como el tiempo que se da entre que se comienza, 
+        se pausa y se termina el tipo de tratamiento en la 
+        peluqueria
+
+    Methods
+    -------
+    muestra_cita()
+        Muestra la cita
+    introduce_cita()
+        Añade una cita
+    """
+
+
     def __init__(self,telefono,dia,turno,tipo_de_cita):
+        """
+        Constructor de la entidad.
+
+        Parameters
+        ----------
+        telefono : str
+        dia : datetime
+        turno : str
+        tipo_de_cita : dict
+        
+        """
         self.telefono=telefono
         self.dia=dia
         self.turno=turno
         self.tipo_de_cita=tipo_de_cita #Esta tiene que ser un diccionario
 
-    def MuestraCita(self):
-        print("Telefono usuario:",self.telefono,"\nDia:",self.dia
-        ,"\nTurno:",self.turno,)
-        print("\nTipo de cita:",self.tipo_de_cita['name'])
-        print("\nTiempo trabajo incial:",self.tipo_de_cita['time']['work1'])
-        print("\nTiempo trabajo pausa:",self.tipo_de_cita['time']['pause'])
-        print("\nTiempo trabajo final:",self.tipo_de_cita['time']['work2'])
+    def get_cita(self):
+        """
+        Obtiene una cita
 
-    def IntroduceCita(self):
-        self.telefono = input("Telefono del usuario: ")
-        self.dia = input("Fecha: ")
-        self.turno = input("Turno: ")
-        self.tipo_de_cita['name'] = input("Tipo de cita: ")
-        self.tipo_de_cita['time']['work1'] = input("Tiempo que se tarda al principio: ")
-        self.tipo_de_cita['time']['pause'] = input("Tiempo de pausa: ")
-        self.tipo_de_cita['time']['work2'] = input("Tiempo para finalizar: ")
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        self : Cita
+            Objeto de tipo cita
+        """
+        return self
+
+    def add_cita(self):
+        
+
+    def addd_cita(self,telefono: str,dia: datetime,turno: str,tipo_de_cita: dict):
+        """
+        Método para añadir una cita
+
+        Parameters
+        ----------
+        telefono : str
+        dia : datetime
+        turno : str
+        tipo_de_cita : dict
+
+        Returns
+        -------
+        None
+        """
+        turno_type = ["maniana","tarde"]
+        if (turno_type in turno) == False:
+            raise AttributeError("El tipo de turno especificado no es correcto.")
+        
+        self.telefono = telefono
+        self.dia = dia
+        self.tipo_de_cita = tipo_de_cita    #Tipo dict
+        
+        
         

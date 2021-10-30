08413827b472ccb1fffe94b39b5aa31aca624bb8
argelion14 <argelion14@gmail.com>
2021-10-30 21:11:47 +0200

    Actualización documentación

--- my_hairdressing_app/Cita.py
+++ my_hairdressing_app/Cita.py
@@ -13,7 +13,7 @@ class tipo_cita(Enum):
 @dataclass(frozen=True)
 class cita:
     """
-    Entidad que representa un objeto valor cita 
+    Objeto valor que representa una cita
 
     Attributes
     ----------

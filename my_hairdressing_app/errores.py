0239f4f5fa4b29934f2c3965f7e5db9c55367e32
Antobio17 <antoniojr997@gmail.com>
2021-11-19 20:09:35 +0100

    (close #27) Nuevos errores. Se ha definido un nuevo tipo de error BadDateFormatError para lanzar excepciones cuando el formato de fecha no es correcto. Se ha definido un nuevo tipo de error ShiftNotExistError para lanzar excepciones cuando el turno especificado no existe.

--- my_hairdressing_app/errores.py
+++ my_hairdressing_app/errores.py
@@ -4,3 +4,40 @@ class PhoneFormatError (Exception):
 class CitaNameNotExist (Exception):
     def __init__(self, message="El tipo de cita especificada no existe"):
         self.message = message
+
+class BadDateFormatError(Exception):
+    """
+    Excepción lanzada para errores si la fecha introducida para la reserva de citas
+    no tiene el formato adecuado.
+
+    Attributes:
+        date -- fecha que ha producido el error.
+        message -- texto explicativo del error.
+    """
+
+    def __init__(self, date, message = "La fecha introducida no está en el formato correcto. Ejemplo (30-10-2021)."):
+        self.date = date
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.date} >> {self.message}'
+
+
+class ShiftNotExistError(Exception):
+    """
+    Excepción lanzada para errores si el turno de trabajo introducida para la reserva de citas
+    no existe.
+
+    Attributes:
+        shift -- turno de trabajo que ha producido el error.
+        message -- texto explicativo del error.
+    """
+
+    def __init__(self, shift, message = "El turno de trabajo introducido no es existe."):
+        self.shift = shift
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.shift} >> {self.message}'

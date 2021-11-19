2ee2ab459eba9b77591a2b9ffe17a86b061b5a7c
Antobio17 <antoniojr997@gmail.com>
2021-11-19 20:11:20 +0100

    Función para lanzar tests. Se ha craedo la función test para ejecutar los tests desde nuestro task manager.

--- tasks.py
+++ tasks.py
@@ -24,3 +24,13 @@ def check(c):
     print("Comprobando sintaxis...")
     run("pyflakes my_hairdressing_app")
     print("Todo correcto. La comprobación ha terminado con éxito!")
+
+@task
+def test(c):
+    """
+    Lanzará todos los tests definidos en el directorio tests
+    """
+
+    print("Lanzando tests...")
+    run("pytest tests/")
+    print("Tests superados con éxito!")

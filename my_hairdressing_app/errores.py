d588515857a764ab5b801e92ba2cc02a01794084
argelion14 <argelion14@gmail.com>
2021-11-19 13:54:51 +0100

    (Close #22) Tipo de error para el telefono Añado un tipo de error para cuando el telefono no sea una cadena

--- my_hairdressing_app/errores.py
+++ my_hairdressing_app/errores.py
@@ -1,10 +1,3 @@
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-class error_cita (Error):
-    
-    def __inti__(self, message):
+class PhoneFormatError (Exception):
+    def __inti__(self, message="El telefono debe ser una cadena"):
         self.message = message
-
-#https://docs.python.org/es/3/tutorial/errors.html#user-defined-exceptions

933723b953090911875d1a499a7f3bb802a6f3f3
Antobio17 <antoniojr997@gmail.com>
2022-01-02 12:19:06 +0100

    :construction:

--- my_hairdressing_app/logging.py
+++ my_hairdressing_app/logging.py
@@ -11,13 +11,13 @@ class Logging:
     """
 
     @staticmethod
-    def info(module_name: str,  message: str):
-        logging.config.dictConfig(Config(True).get_logging_config())
+    def info(module_name: str,  message: str, test = False):
+        logging.config.dictConfig(Config(test).get_logging_config())
         logging.getLogger(module_name).info(message)
 
     @staticmethod
-    def critical(module_name: str, message: str):
-        logging.config.dictConfig(Config(True).get_logging_config())
+    def critical(module_name: str, message: str, test = False):
+        logging.config.dictConfig(Config(test).get_logging_config())
         logging.getLogger(module_name).critical(message)
 
  
